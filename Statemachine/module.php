4931fc990a4e29c3c69b92f40502b2fe87666a61
Adrian Staeves <github@a.staeves.de>
2025-11-19 15:25:27 +0100

    fix

--- Statemachine/module.php
+++ Statemachine/module.php
@@ -116,7 +116,7 @@ class Statemachine extends IPSModule {
 		];
 	}
 	private function statesListForm() {
-		$states = json_decode($this->ReadAttributeString("states"));
+		$states = json_decode($this->ReadAttributeString("states"), true);
 		$maxIndex = empty($states) ? 0 : max(array_keys($states));
 		$allDevicesEmptyList = array_map(function ($x) {return ["deviceID"=>$x, "value"=>""];}, json_decode($this->ReadAttributeString("devices"), true));
 		$this->LogMessage(json_encode($allDevicesEmptyList), 10204);
