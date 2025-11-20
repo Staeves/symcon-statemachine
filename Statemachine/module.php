9df41d189284e39ba366fa8c0e0b9f2387a90eb5
Adrian Staeves <github@a.staeves.de>
2025-11-20 12:33:34 +0100

    fix

--- Statemachine/module.php
+++ Statemachine/module.php
@@ -209,7 +209,7 @@ class Statemachine extends IPSModule {
 							]
 						],
 						"delete" => false,
-						//"values" => $allDevicesEmptyList,
+						"values" => $allDevicesEmptyList,
 						"loadValuesFromConfiguration" => true
 					],
 					"name" => "stateValues",
