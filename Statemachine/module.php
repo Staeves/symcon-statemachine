2e4e67f185872669705742ccf65c17d232f439e9
Adrian Staeves <github@a.staeves.de>
2025-11-20 15:49:46 +0100

    fix

--- Statemachine/module.php
+++ Statemachine/module.php
@@ -71,7 +71,7 @@ class Statemachine extends IPSModule {
 		// update devices list in the settings
 		$this->UpdateFormField("devicesList", "values", json_encode($this->devicesAsListValues($devices)));
 	}
-	public function UpdateNextStateListIndex(array $states) : void {
+	public function UpdateNextStateListIndex(mixed $states) : void {
 		$maxIndex = empty($states) ? 0 : max(array_map(function ($x) {return $x["stateID"];}, iterator_to_array($states)));
 		$this->UpdateFormField("statesList", "columns.0.add", $maxIndex+1);
 	}
