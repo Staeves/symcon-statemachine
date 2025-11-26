bc9cfc5a19eb7f058182e6e290f08e89c96fbbf6
Adrian Staeves <github@a.staeves.de>
2025-11-26 13:06:55 +0100

    fix

--- Statemachine/module.php
+++ Statemachine/module.php
@@ -76,7 +76,7 @@ class Statemachine extends IPSModule {
 		$this->WriteAttributeString("states", json_encode($convertedStates));
 		// if not set to a valide value set active State to the first state
 		$activeState = $this->ReadAttributeString("activeState");
-		if (!array_key_exists($activeState, $convertedStates)) {
+		if (!empty($convertedStates) && !array_key_exists($activeState, $convertedStates)) {
 			$this->WriteAttributeString("activeState", array_keys($convertedStates)[0]);
 		}
 
