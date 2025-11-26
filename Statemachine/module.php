3db6b52d7ad9dd4aa34b137ce480d54f1e85aff2
Adrian Staeves <github@a.staeves.de>
2025-11-26 13:04:28 +0100

    fix

--- Statemachine/module.php
+++ Statemachine/module.php
@@ -247,7 +247,7 @@ class Statemachine extends IPSModule {
 		}
 		foreach ($data as $key => $value) {
 			$combinedValue = array_combine($value["fromGroup"], $value["fromState"]);	// fromState takes precedence for same trigger
-			$this->SetBuffer($key, json_encode($combinedValue);
+			$this->SetBuffer($key, json_encode($combinedValue));
 		}
 	}
 
